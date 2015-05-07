3f74c7c6b60175587867a889194003cf7e24cc48
Russell Mull <russell.mull@puppetlabs.com>
2015-05-06 19:27:06 -0700

    Some data munging

--- lib/main.ex
+++ lib/main.ex
@@ -1,4 +1,26 @@
 defmodule ZeePipe do
+  def clean(s) do
+    String.slice(s, 1..-2)
+  end
+
+  def extract_row(row) do
+    [dna, gender, _, _, _, _, _, _, _, _, _, _, weight, _, _, _, _] = row
+    {clean(dna), clean(gender), clean(weight)}
+  end
+
+  def is_dna_match(dna) do
+    String.contains?(dna, "TAGTAAG")
+  end
+
+  def doit do
+    File.stream!("/tmp/medical_screening_samples.csv")
+      |> Stream.map(fn s -> String.split(s, ",") end)
+      |> Stream.map(&extract_row/1)
+      |> Stream.map(fn {d,g,w} -> {is_dna_match(d),g,w} end)
+      |> Enum.take(5)
+  end
+
+
   def init do
     :ok
   end
@@ -7,3 +29,4 @@ defmodule ZeePipe do
     :not_implemented
   end
 end
+
--- test/zee_pipe_test.exs
+++ test/zee_pipe_test.exs
@@ -1,7 +1,10 @@
 defmodule ZeePipeTest do
   use ExUnit.Case
 
-  test "DNA sequence count" do
-    assert ZeePipe.dna_count("TAGTAAG") == 22154
+  test "my thing" do
+    IO.inspect(ZeePipe.doit())
   end
+  # test "DNA sequence count" do
+  #   assert ZeePipe.dna_count("TAGTAAG") == 22154
+  # end
 end
